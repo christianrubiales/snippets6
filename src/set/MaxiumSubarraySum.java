23835f9f6099adfbc631eba0812e2c962eab573f
christian <christian@christian-Inspiron-15-3552>
2016-06-19 19:18:16 +0800

    MaxiumSubarraySum

---
+++ src/set/MaxiumSubarraySum.java
@@ -0,0 +1,21 @@
+package set;
+
+public class MaxiumSubarraySum {
+
+	static int maxSum(int[] arr) {
+		int maxToHere = arr[0];
+		int maxSoFar = arr[0];
+		
+		for (int i = 1; i < arr.length; i++) {
+			maxToHere = maxToHere + arr[i] > arr[i] ? maxToHere + arr[i] : arr[i];
+			maxSoFar = maxToHere > maxSoFar ? maxToHere : maxSoFar;
+		}
+		
+		return maxSoFar;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(maxSum(new int[]{-3,2,4,1,-3,-4}));//7
+	}
+
+}
